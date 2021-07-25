c54d55a4016c7d9062ca489e297a22ad01328a7a
ccor065 <ccor065@aucklanduni.ac.nz>
2021-07-25 22:02:01 +1200

    Inculded pressure vs other data to visualise the effects of these properties on the pressure. Added .txt files for ease of pulling, incuded the graphs that are output by the python program.

--- project_1/vis_data/plot_data.py
+++ project_1/vis_data/plot_data.py
@@ -7,22 +7,61 @@ import numpy as np
 
 # Year and CO2 injection rate (kg/s)
 year1, co2 = np.genfromtxt('cs_c.txt',delimiter=',',skip_header=1).T
-year2, p= np.genfromtxt('cs_p.txt',delimiter=',',skip_header=1).T
+year2, pressure= np.genfromtxt('cs_p.txt',delimiter=',',skip_header=1).T
+year3, production= np.genfromtxt('cs_q.txt',delimiter=',',skip_header=1).T
+year4, co2_wt= np.genfromtxt('cs_cc.txt',delimiter=',',skip_header=1).T
 
+# Wanting to plot all data against pressure to visualise the effects of each property on the pressure
+# Plot Pressure vs CO2 injection rate
 fig,ax = plt.subplots()
-# Plot the co2 injection rate over time
-lns1 = ax.plot(year1, co2, color='r', label='Net Mass Change')
+# Plot pressure over time
+ax.plot(year2, pressure, 'b--', linewidth = 1, label='Pressure in well')
+# Set the y-axis label
+ax.set_ylabel("Pressure MPa",color="black",fontsize=10)
+
+#create second y-axis
+ax2=ax.twinx()
+ax2.plot(year1, co2, color='r', label='CO2 injection rate')
+# Set the y-axis label
+ax2.set_ylabel('CO2 injection rate kg/s',color="black",fontsize=10)
+
+ax.set_xlabel('Time (year)',fontsize=10)
+ax.set_title('Pressure vs CO2 Injection rate\n')
+plt.savefig('co2-rate_vs_pressure.png',dpi=300)
+plt.show()
+
+# Plot pressure vs production rate over time
+fig,ax = plt.subplots()
+# Plot pressure over time
+ax.plot(year2, pressure, 'b--', linewidth = 1, label='Pressure in well')
+# Set the y-axis label
+ax.set_ylabel("Pressure MPa",color="black",fontsize=10)
+
+#create second y-axis
+ax2=ax.twinx()
+ax2.plot(year3, production, color='r', label='CO2 injection rate')
+# Set the y-axis label
+ax2.set_ylabel('Production Rate kg/s',color="red",fontsize=10)
 
 # Set the x-axis label
 ax.set_xlabel('Time (year)',fontsize=10)
-# Set the y-axis label
-ax.set_ylabel('CO2 injection rate kg/s',color="red",fontsize=10)
+ax.set_title('Pressure vs Production Rate \n')
+plt.savefig('production-rate_vs_pressure.png',dpi=300)
+plt.show()
 
+# Plot pressure vs CO2 wt%
+fig,ax = plt.subplots()
+# Plot pressure over time
+ax.plot(year2, pressure, 'b--', linewidth = 1, label='Pressure in well')
+# Set the y-axis label
+ax.set_ylabel("Pressure MPa",color="black",fontsize=10)
+
+#create second y-axis
 ax2=ax.twinx()
-# Plot the presuure  values over time
-lns2 = ax2.plot(year2, p, 'b--', linewidth = 1, label='Well Injection Rate')
+ax2.plot(year4, co2_wt, color='r', label='CO2 wt%')
 # Set the y-axis label
-ax2.set_ylabel("pressure MPa",color="blue",fontsize=10)
-
-ax.set_title('Pressure vs CO2 Injection rate\n')
+ax2.set_ylabel('CO2wt% ',color="red",fontsize=10)
+ax.set_xlabel('Time (year)',fontsize=10)
+ax.set_title('Pressure vs CO2wt%\n')
+plt.savefig('co2wt_vs_pressure.png',dpi=300)
 plt.show()
