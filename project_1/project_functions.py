e1d3f86092ec5f2336db710a84cdb3e384db4155
ccor065 <ccor065@aucklanduni.ac.nz>
2021-09-07 16:59:22 +1200

    Added qloss to pressure model, updated forecasting

--- project_1/project_functions.py
+++ project_1/project_functions.py
@@ -10,8 +10,42 @@ from load_data import *
 # Define global variables
 TIME, PRESSURE = load_pressure_data()
 STEP = 0.1
+## Information collection
+def save_ode_csv(t, p):
+    """
+    Saves 2-dimensional dataset to CSV file
 
-def get_net_flow(t):
+    Parameters
+    ----------
+    t : array-like
+        independent variable of data set.
+    p : array-like
+        dependent variable of data set.
+
+    """
+
+    modelToSave = np.array([t, p])
+    modelToSave = modelToSave.T
+    np.savetxt("pressureOdeModel.csv", modelToSave, fmt='%.2f,%.4f', header = 't_ode, p_ode')
+def getT_P_ode():
+     """
+#     Solves pressure ode model with calirbated parameters for best fit
+
+#     Returns
+#     -------
+#     t_ode : array of ints/double
+#             independent variable values for which ode solution is numerically solved for
+#     p_ode : array of ints/doubles
+#             array of solutiions for pressure ODE model
+#     """
+     # find correct parameters for a,b and c to fit the model well
+     a, b, c, calibrationPoint = find_pars_pressure()
+     pars = [a,b,c]
+     # solve ode using found parameaters
+     t_ode, p_ode = solve_pressure_ode(pressure_ode_model, TIME[0], PRESSURE[0], TIME[-1], STEP, pars)
+
+     return t_ode, p_ode
+def get_q_dq_conc(t):
     '''
     Returns net flow rate q, and dq/dt, for number of points as in vector input, t.
     Parameters:
@@ -30,7 +64,8 @@ def get_net_flow(t):
     -------
     q_production and q_c02 are interploated and then subtracted to find net flow rate.
     '''
-
+    tc, conc_raw = load_c02_wt_data()
+    conc = np.interp(t, tc, conc_raw)
     # load in flow rate data
     t1, q_raw = load_production_data()
     t2, co2_raw = load_injection_data()
@@ -39,11 +74,10 @@ def get_net_flow(t):
     # as vector t.
     q = np.interp(t, t1, q_raw)
     q_co2 = np.interp(t, t2, co2_raw)
-
     # compute net q
     for i in range(len(t)):
         if t[i] >= 1998.51:
-            q[i] -= q_co2[i]
+            q[i] =  q[i] - q_co2[i]
 
     # numerically differniate q
     dqdt = 0.*q
@@ -51,8 +85,7 @@ def get_net_flow(t):
     dqdt[0] = (q[1]-q[0])/(t[1]-t[0])           #foward differences
     dqdt[-1] = (q[-1]-q[-2])/(t[-1]-t[-2])      #backward differences
 
-    return q, dqdt
-
+    return q, dqdt, conc
 def curve_fit_pressure(t, a, b, c):
     '''
     Solve pressure ode with the given paramters
@@ -78,23 +111,7 @@ def curve_fit_pressure(t, a, b, c):
     timeODE, pressureODE = solve_pressure_ode(pressure_ode_model, t[0], PRESSURE[0], t[-1], STEP, pars)
 
     return pressureODE
-
 def find_pars_pressure():
-    '''
-    Calibrates pressure ODE model to find parameters that provide the best fit
-
-    Returns:
-    --------
-    parameters : array of ints
-                Best fit paramteres for Pressure ODE model
-    trainingSize : int
-                   Data points from data set used to calibrate model
-
-    Notes:
-    --------
-    Elements of parameters are individually returned in index order
-
-    '''
     nt = int(np.ceil((TIME[-1]-TIME[0])/STEP))		# compute number of Euler STEPs to take
     ts = TIME[0]+np.arange(nt+1)*STEP			    # x array
 
@@ -113,108 +130,46 @@ def find_pars_pressure():
 
     return parameters[0], parameters[1], parameters[2], trainingSize
 
-def improved_euler_step(f, tk, yk, h, y0, q, dq, pars):
-	""" Implements the improved euler method for a single step
+# Solvers
+def pressure_ode_model(t, p, p0, dq, q, c_alt, a, b, c):
+    """
+    dgfgdfg
+    """
+    if p > p0:
+        qloss = b/a *(p-p0)*c_alt*t
+        q = q + qloss
 
-        Parameters
-		----------
-		f : callable
-			Derivative function.
-		tk : float
-			Independent variable at beginning of step.
-		yk : float
-			Solution at beginning of step.
-		h : float
-			Step size.
-        q : float
-            value of net flow at a given time, t
-        dq : float
-            value of the derivative of net flow dq/dt at given time, t.
-		pars : iterable
-			Optional parameters to pass to derivative function.
-
-        Returns
-		-------
-		yk1 : float
-			Solution at end of the Euler step.
-	"""
+    dpdt =  -a*q - b*(p-p0) - c*dq
+    return dpdt
+def improved_euler_step(f, tk, yk, h, y0, pars):
 	# y+1 = y0 + h/2 (f(t0, y0) + f(x0+1, y0+1))
-	f0 = f(tk, yk, y0, q, dq, *pars)
-	f1 = f(tk +h , yk + h*f0, y0, q, dq, *pars)
+	f0 = f(tk, yk, y0,  *pars)
+	f1 = f(tk +h , yk + h*f0, y0, *pars)
 	yk1 = yk + 0.5*h*(f0 + f1)
 
 	return yk1
-
-def pressure_ode_model(t, p, p0, q, dq, a, b, c):
-    ''' Return the derivative dq/dt at time, t, for given parameters.
-        Parameters:
-        -----------
-        t : float
-            Independent variable.
-        p : float
-            Dependent variable.
-        p0 : float
-            initial value of dependent variable, pressure.
-        q : float
-            Flow rate at time, t.
-        dq : float
-            value of the derivative of net flow dq/dt at given time, t.
-        a : float
-            parameter into ode
-        b : float
-            parameter into ode
-        c : float
-            parameter into ode
-        Returns:
-        --------
-        dqdt : float
-            Derivative of dependent variable with respect to independent variable.
-    '''
-
-    return -a*q - b*(p-p0) - c*dq
-
-def solve_pressure_ode(f, t0, y0, t1, h, pars=[]):
-    """
-    Compute solution of pressure ODE function using improved euler method.
-
-    Parameters
-	----------
-	f : callable
-		Derivative function.
-	t0 : float
-		Initial value of independent variable.
-	y0 : float
-		Initial value of solution.
-	t1 : float
-		Final value of independent variable.
-	h : float
-		Step size.
-	pars : iterable
-		Optional parameters to pass into derivative function.
-
-    Returns
-	-------
-	xs : array-like
-		Independent variable at solution.
-	ys : array-like
-		Solution.
-
-    Notes
-	-----
-	Assumes that order of inputs to f is f(x,y,*pars).
-    """
-
+def solve_pressure_ode(f, t0, y0, t1, h, pars):
     nt = int(np.ceil((t1-t0)/h))		# compute number of Euler steps to take
     ts = t0+np.arange(nt+1)*h			# x array
     ys = 0.*ts							# array to store solution
     ys[0] = y0                          # set intial value
-    q, dqdt = get_net_flow(ts)
-
+    q, dqdt, c_alt = get_q_dq_conc(ts)
     for i in range(nt):
-        ys[i+1] = improved_euler_step(f, ts[i], ys[i], h, y0, q[i], dqdt[i], pars)
+        pars_2 = [dqdt[i], q[i], c_alt[i], *pars]
+        ys[i+1] = improved_euler_step(f, ts[i], ys[i], h, y0, pars_2)
 
     return  ts, ys
+def solve_pressure_const_q(f, t0, y0, t1, h, pars):
+    # initialise
+    nt = int(np.ceil((t1-t0)/h))		# compute number of Euler steps to take
+    ts = t0+np.arange(nt+1)*h			# x array
+    ys = 0.*ts							# array to store solution
+    ys[0] = PRESSURE[-1]                         # set intial value
+    dqdt = 0
+    for i in range(nt):
+        ys[i+1] = improved_euler_step(f, ts[i], ys[i], h, y0, [dqdt, *pars])
 
+    return  ts, ys
 def analytical_solution(t, q, a, b, c):
     """
     Computes analytical solution for simplified version of pressure ODE model.
@@ -231,104 +186,10 @@ def analytical_solution(t, q, a, b, c):
         p_ana[i] = PRESSURE[0] - ((a * q)/b)*(1 - math.exp(-b*t[i]))
 
     return p_ana
-def solve_pressure_const_q(f, t0, y0, t1, h, q, pars=[]):
-    """
-    Compute solution of the coupled ODE problem using Improved Euler method.
-
-    Parameters
-	----------
-	f : callable
-		Derivative function.
-	t0 : float
-		Initial value of independent variable.
-	y0 : float
-		Initial value of solution.
-	t1 : float
-		Final value of independent variable.
-	h : float
-		Step size.
-	pars : iterable
-		Optional parameters to pass into derivative function.
-
-    Returns
-	-------
-	xs : array-like
-		Independent variable at solution.
-	ys : array-like
-		Solution.
-
-    Notes
-	-----
-	Assumes that order of inputs to f is f(x,y,*pars).
-    """
-
-    # initialise
-    nt = int(np.ceil((t1-t0)/h))		# compute number of Euler steps to take
-    ts = t0+np.arange(nt+1)*h			# x array
-    ys = 0.*ts							# array to store solution
-    ys[0] = y0                          # set intial value
-    dqdt = 0                            # ignore dqdt for constant
-
-    for i in range(nt):
-        ys[i+1] = improved_euler_step(f, ts[i], ys[i], h, y0, q, dqdt, pars)
-
-    return  ts, ys
-
-def save_ode_csv(t, p):
-    """
-    Saves 2-dimensional dataset to CSV file
-
-    Parameters
-    ----------
-    t : array-like
-        independent variable of data set.
-    p : array-like
-        dependent variable of data set.
-
-    """
-
-    modelToSave = np.array([t, p])
-    modelToSave = modelToSave.T
-    np.savetxt("pressureOdeModel.csv", modelToSave, fmt='%.2f,%.4f', header = 't_ode, p_ode')
-
-# def getT_P_ode():
-#     """
-#     Solves pressure ode model with calirbated parameters for best fit
-
-#     Returns
-#     -------
-#     t_ode : array of ints/double
-#             independent variable values for which ode solution is numerically solved for
-#     p_ode : array of ints/doubles
-#             array of solutiions for pressure ODE model
-#     """
-#     # find correct parameters for a,b and c to fit the model well
-#     a, b, c, calibrationPoint = find_pars_pressure()
-#     pars = [a,b,c]
-#     # solve ode using found parameaters
-#     t_ode, p_ode = solve_pressure_ode(pressure_ode_model, TIME[0], PRESSURE[0], TIME[-1], STEP, pars)
-
-#     return t_ode, p_ode
-
 def plot_pressure_benchmark():
-    '''
-    Compare analytical and numerical solutions.
-
-    Parameters:
-    -----------
-    none
-
-    Returns:
-    --------
-    none
-
-    Notes:
-    ------
-    This function called within if __name__ == "__main__":
-    It should contain commands to obtain analytical and numerical solutions,
-    plot these, and either display the plot to the screen or save it to the disk.
-    '''
-
+    """
+    dsfds
+    """
     #Configure plots
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.set_figwidth(10)
@@ -350,8 +211,7 @@ def plot_pressure_benchmark():
     ax1.plot(TIME, PRESSURE,color='k', label =' Observations best fit')
     ax1.scatter(TIME, PRESSURE,color='k', marker = 'x', label ='Observations')
     ax1.axvline(t_ode[calibrationPoint], linestyle = '--', label = 'Calibration Point')
-    q, dq = get_net_flow(t_ode)
-    print(q)
+
     # plot the model solution
     ax1.plot(t_ode, p_ode, color = 'r', label = 'ODE')
     ax1.set_title('ODE vs Data')
@@ -366,7 +226,9 @@ def plot_pressure_benchmark():
     # get average net production rate
     q = 4
     time = np.linspace(0, 50, 100)
-    t_odeA, p_odeA = solve_pressure_const_q(pressure_ode_model, time[0], PRESSURE[0], time[-1], STEP, q, pars)
+    conc = 0.03
+    pars_2 = [conc, q, *pars]
+    t_odeA, p_odeA = solve_pressure_const_q(pressure_ode_model, time[0], PRESSURE[0], time[-1], STEP, pars_2)
     p_ana = analytical_solution(time, q, a, b, c)
     ax2.plot(t_odeA, p_odeA, color = 'r', label = 'ODE')
     ax2.scatter(time, p_ana, color = 'b', label = 'Analytical Solution')
@@ -435,15 +297,16 @@ def plot_individual_injRate(t, pars, injRate, color,  description):
         # load in flow rate data
         t1, q_raw = load_production_data()
         t2, co2_raw = load_injection_data()
+        t3, conc_raw = load_c02_wt_data()
         # get net flow at final time.
         q = q_raw[-1] - (co2_raw[-1])*injRate
+        conc = conc_raw[-1]
+        print(conc)
+        print(t3[-1], t1[-1], t2[-1])
 
-        t, p = solve_pressure_const_q(pressure_ode_model, t[0], PRESSURE[-1], t[-1], STEP, q, pars)
+        t, p = solve_pressure_const_q(pressure_ode_model, t[0], PRESSURE[0], t[-1], STEP, [q, conc, *pars])
         plt.plot(t, p,color=color,  label =description)
         return
-
-
-
 def plot_model_predictions():
 
     a, b, c, _ = find_pars_pressure()
@@ -462,7 +325,7 @@ def plot_model_predictions():
     endTime = TIME[-1] + 30                     # 30 years projection
     nt = int(np.ceil((endTime-TIME[-1])/STEP))	# compute number of Euler steps to take
     ts = TIME[-1]+np.arange(nt+1)*STEP			# x array
-
+    print(pars)
     ##### CHANGES
     # stop injection
     plot_individual_injRate(ts, pars, 0,  'orange',  'Stop injection')
@@ -474,15 +337,10 @@ def plot_model_predictions():
     # double injection
     # quadruple injection
     plot_individual_injRate(ts, pars, 4,  'cyan',  'Quadruple injection')
-    plt.legend()
+    #plt.legend()
     plt.show()
     return
-
-
-
-
 if __name__ == "__main__":
     #plot_pressure_benchmark()
-    a, b, c, _ = find_pars_pressure();
-    print(a, b, c)
+
     plot_model_predictions()
