4b546662798dd74e3ce6f4a7ea8e259cdd96255d
ccor065 <ccor065@aucklanduni.ac.nz>
2021-09-08 17:16:15 +1200

    Added comments to pressure functions in lmp_model_functions.py

--- project_1/lpm_model_functions.py
+++ project_1/lpm_model_functions.py
@@ -13,7 +13,7 @@ STEP = 0.1
 ### PRESSURE FUNCTIONs
 """
 ### Curve fitting functions
-def curve_fit_pressure(t, a, b, c):
+def curve_fit_pressure(t, a, b, c): #comment done
     '''
     Solve pressure ode with the given paramters
 
@@ -35,28 +35,51 @@ def curve_fit_pressure(t, a, b, c):
     '''
 
     pars = [a, b, c]
+    # get numerical solution to ODE using specific pars.
     timeODE, pressureODE = solve_pressure_ode(pressure_ode_model, t[0], PRESSURE[0], t[-1], STEP, pars)
 
     return pressureODE
-def find_pars_pressure():
-    nt = int(np.ceil((TIME_P[-1]-TIME_P[0])/STEP))		# compute number of Euler STEPs to take
-    ts = TIME_P[0]+np.arange(nt+1)*STEP			    # x array
+def find_pars_pressure():#comment done
+    '''
+    finds the specific parameters for the ODE solver using scipy curve_fit.
 
-    # initial guesses
-    a = 0.001 #0.001
-    b = 0.09 #0.09
-    c = 0.003 #0.005
+    Parameters:
+    -----------
+    t : array-like
+        array of time values
+    a : float
+         value of parameter a
+    b : float
+        value of parameter b
+    c : float
+        value of parameter c
+
+    Returns:
+    --------
+    pressureODE : array-like
+                numerical solution to the pressure ODE given the specific parameters
+    '''
+    # initalise time array
+    nt = int(np.ceil((TIME_P[-1]-TIME_P[0])/STEP))	# get number of time points
+    ts = TIME_P[0]+np.arange(nt+1)*STEP			    # initial time array
+
+    # initial parameter guesses
+    a = 0.001
+    b = 0.09
+    c = 0.003
     pars = [a, b, c]
 
     # make input pressure same length as the output from the solver
     pi = np.interp(ts, TIME_P, PRESSURE)
 
-    # find parameters
-    trainingSize = math.ceil(0.8*len(ts))
-    parameters, covariance = curve_fit(curve_fit_pressure, ts[0:trainingSize], pi[0:trainingSize], pars)
 
+    trainingSize = math.ceil(0.8*len(ts)) # get length training array
+
+    # use curve_fit to find pars that give best fit ODE to data.
+    parameters, covariance = curve_fit(curve_fit_pressure, ts[0:trainingSize], pi[0:trainingSize], pars)
+    # return pars, a, b, c and also calibration point.
     return parameters[0], parameters[1], parameters[2], trainingSize
-def get_q_dq_conc(t):
+def get_q_dq_conc(t):#comment done
     '''
     Returns net flow rate q, and dq/dt, for number of points as in vector input, t.
     Parameters:
@@ -66,26 +89,31 @@ def get_q_dq_conc(t):
 
     Returns:
     --------
-    q : array-like
-        net flow rate
-    dq/dt: : array-like
-        derivative of net flow rate
+    q:     array-like
+           net flow rate
+    dqdt:  array-like
+           derivative of net flow rate
+    conc:  array-like
+           interpolated concentration for time array, t.
 
     Notes:
     -------
     q_production and q_c02 are interploated and then subtracted to find net flow rate.
     '''
+    # load in flow rate and concentration data
     tc, conc_raw = load_c02_wt_data()
-    conc = np.interp(t, tc, conc_raw)
-    # load in flow rate data
     t1, q_raw = load_production_data()
     t2, co2_raw = load_injection_data()
+
+    #interpolate concentration
+    conc = np.interp(t, tc, conc_raw)
 
     # Interpolate co2 injection and production vectors to have same amount of points
     # as vector t.
     q = np.interp(t, t1, q_raw)
     q_co2 = np.interp(t, t2, co2_raw)
-    # compute net q
+
+    # compute net flow rate
     for i in range(len(t)):
         if t[i] >= 1998.51:
             q[i] =  q[i] - q_co2[i]
@@ -103,6 +131,18 @@ def pressure_analytical_solution(t, q, a, b, c):
     """
     Computes analytical solution for simplified version of pressure ODE model.
     Used for bench marking.
+    Parameters:
+    -----------
+    t : array-like
+        array of time values
+    q : float
+        value of constant net #flow rate
+    a : float
+         value of parameter a
+    b : float
+        value of parameter b
+    c : float
+        value of parameter c
 
     Returns
     -------
@@ -114,38 +154,163 @@ def pressure_analytical_solution(t, q, a, b, c):
     for i in range(len(t)):         # compute analtical solution
         p_ana[i] = PRESSURE[0] - ((a * q)/b)*(1 - math.exp(-b*t[i]))
 
-    return p_ana
+    return p_ana# comment done
 #### MODEL FUNCTIONS
 def pressure_ode_model(t, p, p0, dq, q, conc, a, b, c):
-    """
-    dgfgdfg
-    """
+    '''
+    Computes dp/dt at a specific time given parameters
+
+    Parameters:
+    -----------
+    t : float
+        time at which dp/dt is computed
+    p : float
+         value of pressure at time, t.
+    p0 : float
+        initial pressure
+    dq : float
+        vlalue of the derivative of net flow rate dq/dt at time t.
+    q : float
+         net flow at time t.
+    conc : float
+        concentration at time t.
+    a : float
+         value of parameter a
+    b : float
+        value of parameter b
+    c : float
+        value of parameter c
+
+    Returns:
+    --------
+    dpdt : float
+           value of the derivate dp/dt at time t.
+    '''
+    # calculate ground water loss if pressure is greater than inital pressure.
     if p > p0:
         qloss = (b/a) *(p-p0)*conc*t
-        q = q + qloss
-    dpdt =  -a*q - b*(p-p0) - c*dq
-    return dpdt
+        q = q + qloss #q loss reduces qc02 therefore add to net flow.
+    dpdt =  -a*q - b*(p-p0) - c*dq  # calculate derivative# #
+    return dpdt#comment done
 #### numerical Solvers
-def improved_euler_step(f, tk, yk, h, y0, pars):
-    # y+1 = y0 + h/2 (f(t0, y0) + f(x0+1, y0+1))
+def improved_euler_step(f, tk, yk, h, y0, pars): # comment done
+    '''
+    Computes dp/dt at a specific time given parameters
+
+    Parameters:
+    -----------
+    f : function
+        ODE model function to compute derivative.
+    tk : float
+         value of time one increment before estimation time.
+    yk : float
+        value of dependent variable one increment beofe solver.
+    h : float
+        step-size, indcates value to incremnt independent variable
+    y0 : float
+        inital value of dependent variable.
+    pars : array-like
+        individual paramters to be passed into ode function.
+
+
+    Returns:
+    --------
+    yk1 : float
+          Value of the dependent variable at time = tk + 1(increment).
+    Notes:
+    --------
+    Improved euler step uses this equation to compute its prediction.
+     y+1 = y0 + h/2 (f(t0, y0) + f(x0+1, y0+1))
+    '''
+    # Compute improved euler # # step
     f0 = f(tk, yk, y0,  *pars)
     f1 = f(tk +h , yk + h*f0, y0, *pars)
     yk1 = yk + 0.5*h*(f0 + f1)
 
     return yk1
 
-def solve_pressure_ode(f, t0, y0, t1, h, pars):
+def solve_pressure_ode(f, t0, y0, t1, h, pars):# comment done
+    '''
+    Computes dp/dt at a specific time given parameters
+
+    Parameters:
+    -----------
+    f : function
+        ODE model function to compute derivative.
+    t0 : float
+         Inital value of time to solve ode from
+    y0 : float
+        Inital value of dependent variable.
+    t1 : float
+        final value of independent variable.
+    h : float
+        step-size, indcates value to increment independent variable
+    pars : array-like
+        individual paramters that are solved using the curve_fit function.
+        pars should contain: [a, b, c] only, where a,b,c are solved using
+        curve_fit.
+
+    Returns:
+    --------
+    ts : array-like
+         Corresponding times at which the dependent variables are solved.
+    ys : array-like
+         Solution to ODE for given time range.
+
+    Notes:
+    --------
+    The parameter 'pars' does not contain the same pars as what is passed into
+    Improved euler step function nor the ODE model function. These are only the
+    parameters which need to be found using curve_fit.
+    '''
     nt = int(np.ceil((t1-t0)/h))		# compute number of Euler steps to take
     ts = t0+np.arange(nt+1)*h			# x array
     ys = 0.*ts							# array to store solution
     ys[0] = y0                          # set intial value
-    q, dqdt, c_alt = get_q_dq_conc(ts)
-    for i in range(nt):
-        pars_2 = [dqdt[i], q[i], c_alt[i], *pars]
-        ys[i+1] = improved_euler_step(f, ts[i], ys[i], h, y0, pars_2)
 
+    # get q, dqdt and concentration arrays (same length as ts)
+    q, dqdt, conc = get_q_dq_conc(ts)
+    for i in range(nt):
+        # compute improved euler step to solve the ODE numerically
+        ys[i+1] = improved_euler_step(f, ts[i], ys[i], h, y0, [dqdt[i], q[i], conc[i], *pars])
+
+    # return arays containing the time and solved dependent variables.
     return  ts, ys
-def solve_pressure_const_q(f, t0, y0, t1, h, pars):
+def solve_pressure_const_q(f, t0, y0, t1, h, pars):# comment done
+    '''
+    Computes solution to pressure ODE using improved euler.
+
+    Parameters:
+    -----------
+    f : function
+        ODE model function to compute derivative.
+    t0 : float
+         Inital value of time to solve ode from
+    y0 : float
+        Inital value of dependent variable.
+    t1 : float
+        final value of independent variable.
+    h : float
+        step-size, indcates value to increment independent variable
+    pars : array-like
+        individual paramters that are solved using the curve_fit function.
+        pars should contain = [conc, q, a, b, c].
+        where a,b,c are solved using curve_fit and conc and q are contstant values
+        of concentration wt%C02 and net flow rate respectively.
+
+    Returns:
+    --------
+    ts : array-like
+         Corresponding times at which the dependent variables are solved.
+    ys : array-like
+         Solution to ODE for given time range.
+
+    Notes:
+    --------
+    The parameter 'pars' does not contain the same pars as what is passed into
+    Improved euler step function nor the ODE model function. These are only the
+    parameters which need to be found using curve_fit.
+    '''
     # initialise
     nt = int(np.ceil((t1-t0)/h))		# compute number of Euler steps to take
     ts = t0+np.arange(nt+1)*h			# x array
@@ -160,12 +325,29 @@ def solve_pressure_const_q(f, t0, y0, t1, h, pars):
 ### Concentration FUNCTIONs
 '''
 ### Curve fitting functions
-def curve_fit_conc(t, d, m0):
+def curve_fit_conc(t, d, m0):#comment done
+    '''
+    Solve pressure ode with the given paramters
+
+    Parameters:
+    -----------
+    t : array-like
+        array of time values
+    d : float
+         value of parameter d
+    m0 : float
+        value of parameter initial mass of system
+    Returns:
+    --------
+    concODE : array-like
+            numerical solution to the concentration ODE given the specific parameters
+    '''
+
     a,b, __,_ = find_pars_pressure()
     pars = [a, b, d, m0]
     p0 = PRESSURE[0]
-    time, conc = solve_conc_ode(conc_ODE_model, t[0], CONC[0], t[-1], STEP, p0, pars)
-    return conc
+    time, concODE = solve_conc_ode(conc_ODE_model, t[0], CONC[0], t[-1], STEP, p0, pars)
+    return concODE
 def find_pars_conc():
 
     nt = int(np.ceil((TIME_C[-1]-TIME_C[0])/STEP))		# compute number of Euler STEPs to take
