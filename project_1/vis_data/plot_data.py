360d1f48cab690ac0dffc9ab2f16f12815bcd693
ccor065 <ccor065@aucklanduni.ac.nz>
2021-09-12 21:37:49 +1200

    did some more data graphing

--- project_1/vis_data/plot_data.py
+++ project_1/vis_data/plot_data.py
@@ -6,10 +6,70 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # Year and CO2 injection rate (kg/s)
-year1, co2 = np.genfromtxt('cs_c.txt',delimiter=',',skip_header=1).T
+year1, co2= np.genfromtxt('cs_c.txt',delimiter=',',skip_header=1).T
 year2, pressure= np.genfromtxt('cs_p.txt',delimiter=',',skip_header=1).T
 year3, production= np.genfromtxt('cs_q.txt',delimiter=',',skip_header=1).T
 year4, co2_wt= np.genfromtxt('cs_cc.txt',delimiter=',',skip_header=1).T
+
+fig,ax = plt.subplots()
+fig.set_figwidth(8)
+# Plot pressure over time
+ax.plot(year2, pressure, 'black', linewidth = 1, label='Pressure in well')
+# Set the y-axis label
+ax.set_ylabel("Pressure MPa",color="black",fontsize=10)
+#create second y-axis
+ax2=ax.twinx()
+ax2.plot(year1, co2, color='r', label='CO2 Injection Rate [kg/s]')
+ax2.plot(year3, production,  'b--', label= "Extraction Rate [kg/s]")
+# Set the y-axis label
+ax2.set_ylabel('Flow rate kg/s',color="r",fontsize=10)
+ax.axvline(year1[0], linestyle = ':', label = 'Injection Begins')
+
+ax.set_xlabel('Time (year)',fontsize=10)
+ax.set_title("Comparison of Pressure to Extraction rate and Injection rate \n in the Ohaaki Geothermal Reservoir")
+fig.legend(bbox_to_anchor =(0.9,0.88))
+plt.savefig('pressure_flow_rates.png',dpi=300)
+plt.show()
+
+
+fig,ax = plt.subplots()
+# Plot pressure over time
+# Set the y-axis label
+ax.set_ylabel("CO2 Injection Rate [kg/s",color="black",fontsize=10)
+#create second y-axis
+ax2=ax.twinx()
+ax.plot(year1, co2, color='r', label='CO2 Injection Rate [kg/s]')
+ax2.plot(year4, co2_wt,  'b--', label= "C02 [wt fraction]")
+# Set the y-axis label
+ax2.set_ylabel('CO2wt% ',color="red",fontsize=10)
+ax.set_xlabel('Time (year)',fontsize=10)
+ax.axvline(year1[0], linestyle = ':', label = 'Injection Begins')
+
+ax.set_ylim(0, 80)
+ax.set_title("Comparison of C02 Concentration and CO2 injection.")
+fig.legend(bbox_to_anchor =(0.9,0.9))
+plt.savefig('c02wt_vs_injection.png',dpi=300)
+plt.show()
+
+'''
+
+fig,ax = plt.subplots()
+# Plot pressure over time
+ax.plot(year2, pressure, 'black', linewidth = 1, label='Pressure in well')
+# Set the y-axis label
+ax.set_ylabel("Pressure MPa",color="black",fontsize=10)
+#create second y-axis
+ax2=ax.twinx()
+ax2.plot(year1, co2, color='r', label='CO2 Injection Rate [kg/s]')
+ax2.plot(year3, production,  'b--', label= "Extraction Rate [kg/s]")
+# Set the y-axis label
+ax2.set_ylabel('Flow rate kg/s',color="r",fontsize=10)
+ax.set_xlabel('Time (year)',fontsize=10)
+ax.set_title("Comparison of Pressure to Extraction rate and Injection rate \n in the Ohaaki Geothermal Reservoir")
+fig.legend(bbox_to_anchor =(0.9,0.9))
+plt.savefig('pressure_flow_rates.png',dpi=300)
+plt.show()
+
 
 # Wanting to plot all data against pressure to visualise the effects of each property on the pressure
 # Plot Pressure vs CO2 injection rate
@@ -66,3 +126,4 @@ ax.set_title('CO2 Injection Rate vs CO2wt%\n')
 fig.legend()
 plt.savefig('co2wt_vs_pressure.png',dpi=300)
 plt.show()
+'''
